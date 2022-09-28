0af373bf987c6e129c4d0daf5917d7383c2fd94d
Chuhj <chj9574@naver.com>
2022-09-28 19:37:31 +0900

    기본 타입

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,29 @@
+// ts 문자열
+let str: string = 'hello';
+
+// ts 숫자
+let num: number = 0;
+
+// ts 배열
+let arr: Array<number> = [1, 2, 3];
+let strs: Array<string> = ['a', 'b', 'c'];
+let items: number[] = [1, 2, 3];
+
+// ts 튜플
+let address: [string, number] = ['gangnam', 100];
+
+// ts 객체
+let obj: object = {};
+
+// let person: object = {
+//   name: 'a',
+//   age: 100,
+// };
+
+let person: { name: string; age: number } = {
+  name: 'b',
+  age: 100,
+};
+
+// ts 불린
+let show: boolean = true;
